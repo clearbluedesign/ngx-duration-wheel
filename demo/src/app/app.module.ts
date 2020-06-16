546356f9af5822845a0750473e8d625d272d3b46
Roman Popovici <lambidu@live.com>
2020-06-16 12:05:42 +0300

    add (demo): preview of duration wheel

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxDurationWheelModule } from 'ngx-duration-wheel';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		NgxDurationWheelModule
+	],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
